6272f3eb4e3853f66f2691cc6d1f31a8724b40bf
b09704066 <75825794+b09704066@users.noreply.github.com>
2022-01-04 13:41:01 +0800

    Update ball.h

--- ball.h
+++ ball.h
@@ -1,1 +1,44 @@
+//
+// Created by Nina on 2021/12/30.
+//
+
+#ifndef BALL_H
+#define BALL_H
+#include <SFML/Graphics.hpp>
+
+class Ball
+{
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Vector2f serverPos;
+
+
+//private functions
+
+    void initTexture();
+    void initSprite();
+
+public:
+    sf:: Vector2f bounceVelocity;
+    float gravity;
+    float rotateAngle;
+    sf::FloatRect getGlobalBounds();
+    sf::Vector2f getPosition();
+
+    Ball();
+    ~Ball();
+
+//functions
+    void updateWindowBoundsCollision(const sf::RenderTarget& target);
+    void velocityChange(const sf::RenderTarget& target);
+    void bounce(const sf::RenderTarget& target);
+    void rotate(const sf::RenderTarget& target);
+    void update(const sf::RenderTarget& target);
+    void render(sf::RenderTarget& target);
+
+
+};
+
+#endif BALL_H
 
