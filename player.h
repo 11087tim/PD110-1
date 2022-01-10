97cb5e0e5c67818350ffb33f5a668eac9e992d3f
chennnnnnn <80166253+11087tim@users.noreply.github.com>
2022-01-10 21:10:05 +0800

    Update player.h

--- player.h
+++ player.h
@@ -3,21 +3,24 @@
 
 # include "Entity.h"
 
-class Player : public Entity{
+class Player {
 
     private:
 
+        // picture resource
+        sf::Sprite* sprite ;
+        sf::Texture* texture;
+
         // variable
         int character; //1 for Jie, 2 for CMK
-        float gravity;
-        float directionY;
-        float jumpSpeed;
+        float gravity = .3f;
+        float movementSpeed = 3.f;
+        float jumpSpeed = -5.f;
 
-        // function
+
+    // function
         void initTexture(int character);
         void initSprite(int character, sf::RenderTarget* target);
-
-
 
     public:
 
@@ -33,10 +36,12 @@ class Player : public Entity{
         const bool checkCollision();
         float getJumpSpeed();
         void jump();
+        void move(const float dir_x, const float dir_y);
         void velocityChange(const sf:: RenderTarget* target);
         void updateWindowBoundCollision(const sf::RenderTarget* target);
         void reset(int character, const sf::RenderTarget* target);
         void update(const float&dt, const sf::RenderTarget* target);
+        void render(sf::RenderTarget* taget);
 
 
 };
