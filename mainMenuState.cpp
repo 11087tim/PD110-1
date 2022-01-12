b3511fa6bf533bf6d59aa60645e0f3bf3d5b4f3d
b09704066 <75825794+b09704066@users.noreply.github.com>
2022-01-13 00:29:50 +0800

    Update mainMenuState.cpp

--- mainMenuState.cpp
+++ mainMenuState.cpp
@@ -1,4 +1,8 @@
-#include "mainMenuState.h"
+//
+// Created by Nina on 2022/1/10.
+//
+
+#include "MainMenuState.h"
 
 // constructor
 mainMenuState::mainMenuState(sf::RenderWindow* window, std:: stack<State*>* states) : State(window, states)  // constructor
@@ -28,7 +32,7 @@ mainMenuState::~mainMenuState(){
 // functions
 void mainMenuState::initMusic() {
 
-    if(!this->music.openFromFile("pickachu.ogg")){
+    if(!this->music.openFromFile("/Users/nina/Desktop/C++practice/Game/pickachu.ogg")){
         std::cout << "BGM failed to open" << "\n";
     }
     music.setVolume(40);
@@ -53,20 +57,30 @@ void mainMenuState::initBanner() {
 }
 
 void mainMenuState::initDecoration() {
-    if(!this->decoration_t1.loadFromFile("ACgirl.png")){
+    if(!this->decoration_t1.loadFromFile("/Users/nina/Desktop/C++practice/Game/texture/ACgirl.png")){
         std:: cout << "AC_girl.png loading issue !!!" << "\n";
     }
     decoration_s1.setTexture(decoration_t1);
     decoration_s1.setPosition(1840, 250);
 
-
-
-    if(!this->decoration_t2.loadFromFile("ywTsai.png")){
+    if(!this->decoration_t2.loadFromFile("/Users/nina/Desktop/C++practice/Game/texture/ywTsai.png")){
         std:: cout << "ywTsai.png loading issue !!!" << "\n";
     }
     decoration_s2.setTexture(decoration_t2);
-    decoration_s2.setPosition(50, 250);
+    decoration_s2.setPosition(100, 230);
     decoration_s2.scale(0.2f, 0.2f);
+    this->decoDir = 1.f;
+
+    if(!this->decoration_t3.loadFromFile("/Users/nina/Desktop/C++practice/Game/texture/pdogs.png")){
+        std:: cout << "pdogs.png loading issue !!!" << "\n";
+    }
+    decoration_s3.setTexture(decoration_t3);
+    decoration_s3.setPosition(0, 2000);
+    decoration_s3.scale(.15f, .15f);
+    this->ballDirX = 1.f;
+    this->ballDirY = 1.f;
+    this->ballGravity = .1f;
+    this->ballVelocity = sf::Vector2f(4.f, 2.f);
 
 
 }
@@ -95,7 +109,7 @@ void mainMenuState::initButton() {
 }
 
 void mainMenuState::initFont(){  // loadFont
-    if(!this->font.loadFromFile("VCR_OSD_MONO_1.001.ttf")){
+    if(!this->font.loadFromFile("/Users/nina/Desktop/C++practice/Game/font/VCR_OSD_MONO_1.001.ttf")){
         throw("ERROR::MAINMENUSTATE::COUNLD NOT LOAD FONT.");
     }
 }
@@ -122,7 +136,7 @@ void mainMenuState::updateButton() {
 
     }
 
-     //QUIT GAME
+    //QUIT GAME
     if(this->button["EXIT_STATE"]->isPressed()){
         this->music.pause();
         this-> wantEnd = true;
@@ -130,13 +144,74 @@ void mainMenuState::updateButton() {
 
 }
 
+void mainMenuState:: updateImageAnimation()
+{
+    imageAnimation();
+    imageMove();
+}
+
+void mainMenuState:: imageMove()
+{
+    this-> decoration_s1.move(0.f, this->decoDir);
+    this->decoration_s2.move(0.f, this->decoDir);
+}
+
+void mainMenuState::imageAnimation() {
+    if(this->decoration_s1.getGlobalBounds().top < 230.f)
+    {
+        this->decoDir = 1.f;
+    }
+    else if(this->decoration_s1.getGlobalBounds().top > 250.f)
+    {
+        this->decoDir = -1.f;
+    }
+}
+
+void mainMenuState::ballmove() {
+    this->decoration_s3.move(this->ballVelocity.x * this-> ballDirX, this->ballVelocity.y * this->ballDirY);
+}
+
+void mainMenuState:: ballVelocityChange()
+{
+    this-> ballVelocity.y += ballGravity;
+    if(this->decoration_s3.getPosition().y < 2000 * 2/3 && this->ballDirY == -1.f)
+        this->ballVelocity.y +=  -2 * ballGravity;
+//    else if(this->decoration_s3.getPosition().y > 2000 * 2/3 && this->ballDirY == 1.f)
+//        this-> ballVelocity.y += 2 * ballGravity;
+}
+
+void mainMenuState::ballCollision() {
+    if(this->decoration_s3.getGlobalBounds().left <= 0.f)
+    {
+        this->ballDirX = 1.f;
+        this->decoration_s3.setPosition(0, this->decoration_s3.getPosition().y);
+    }
+    else if(this->decoration_s3.getGlobalBounds().left + this->decoration_s3.getGlobalBounds().width >= 2500)
+    {
+        this->ballDirX = -1.f;
+        this->decoration_s3.setPosition(2500 - this->decoration_s3.getGlobalBounds().width, this->decoration_s3.getPosition().y);
+    }
+    else if(this->decoration_s3.getGlobalBounds().top + this->decoration_s3.getGlobalBounds().height >= 2000.f)
+    {
+        this-> ballDirY = -1.f;
+        this->decoration_s3.setPosition(this->decoration_s3.getPosition().x, 2000 - this->decoration_s3.getGlobalBounds().height);
+    }
+
+}
+
+void mainMenuState:: updateBall()
+{
+    this->ballVelocityChange();
+    this->ballCollision();
+    this->ballmove();
+}
+
 void mainMenuState::update(const float& dt){  //
+    this->updateImageAnimation();
+    this->updateBall();
     this->updateMousePosition();
     this-> updateInput(dt);  // weather ESC is pressed
     this->updateButton();  // update buttons status
-
-
-
 }
 
 void mainMenuState::renderButtons(sf::RenderTarget *target) {
@@ -157,6 +232,7 @@ void mainMenuState:: render(sf::RenderTarget* target){
     target->draw(this->banner_text);
     target->draw(this->decoration_s1);
     target->draw(this->decoration_s2);
+    target->draw(this->decoration_s3);
     this->renderButtons(target);
 
 }
