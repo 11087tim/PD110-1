22d3ccde89e6c7ad89955dc94aaefa3b2703a010
b09704066 <75825794+b09704066@users.noreply.github.com>
2022-01-13 00:31:07 +0800

    Update player.h

--- player.h
+++ player.h
@@ -4,46 +4,41 @@
 # include "Entity.h"
 
 class Player {
+private:
+    // picture resource
+    sf::Sprite* sprite ;
+    sf::Texture* texture;
 
-    private:
-
-        // picture resource
-        sf::Sprite* sprite ;
-        sf::Texture* texture;
-
-        // variable
-        int character; //1 for Jie, 2 for CMK
-        float gravity = .3f;
-        float movementSpeed = 3.f;
-        float jumpSpeed = -5.f;
-
+    // variable
+    int character; //1 for Jie, 2 for CMK
+    float gravity = .3f;
+    float movementSpeed = 5.f;
+    float jumpSpeed = -5.f;
+    bool isjumping = false;
 
     // function
-        void initTexture(int character);
-        void initSprite(int character, sf::RenderTarget* target);
+    void initTexture(int character);
+    void initSprite(int character, sf::RenderTarget* target);
 
-    public:
+public:
+    // constructor
+    Player(int character, sf::RenderTarget* target);
+    // destructor
+    ~Player();
 
-        // constructor
-        Player(int character, sf::RenderTarget* target);
-        // destructor
-        ~Player();
-
-
-        //functions
-        sf::FloatRect getGlobalBounds();
-        sf::Vector2f getPosition();
-        const bool checkCollision();
-        float getJumpSpeed();
-        void jump();
-        void move(const float dir_x, const float dir_y);
-        void velocityChange(const sf:: RenderTarget* target);
-        void updateWindowBoundCollision(const sf::RenderTarget* target);
-        void reset(int character, const sf::RenderTarget* target);
-        void update(const float&dt, const sf::RenderTarget* target);
-        void render(sf::RenderTarget* taget);
-
-
+    //functions
+    sf::Rect<float> getGlobalBounds();
+    sf::Vector2f getPosition();
+    const bool checkCollision();
+    float getJumpSpeed();
+    void jump();
+    void move(const float dir_x, const float dir_y);
+    void velocityChange(const sf:: RenderTarget* target);
+    void updateWindowBoundCollision(const sf::RenderTarget* target);
+    void counterEnemy(float enemyPosX, float enemyPosY, float enemyWidth, sf::RenderTarget* target);
+    void reset(int character, const sf::RenderTarget* target);
+    void update(const float&dt, const sf::RenderTarget* target);
+    void render(sf::RenderTarget* taget);
 };
 
 
