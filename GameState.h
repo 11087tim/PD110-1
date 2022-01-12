3da35eef0f6588983f5249249182b645bdf2c5c3
b09704066 <75825794+b09704066@users.noreply.github.com>
2022-01-13 00:27:53 +0800

    Update GameState.h

--- GameState.h
+++ GameState.h
@@ -4,41 +4,53 @@
 #include "State.h"
 #include "player.h"
 #include "ball.h"
+#include "Enemies.h"
 
 class GameState: public State{
-    private:
-        // character
-        Player* playerJie;
-        Player* playerCMK;
+private:
+    // character
+    Player* playerJie;
+    Player* playerCMK;
 
-        // background
-        sf::Sprite sprite;
-        sf::Texture texture;
+    // background
+    sf::Sprite sprite;
+    sf::Texture texture;
 
-        //object
-        Ball* ball;
+    //object
+    Ball* ball;
 
-        // function
-        void initCharacter();
-        void initBall();
-        void initTexture();
-        void initSprite(sf:: RenderTarget* target);
-        void initBackGround();
+    //Enemies
+    Enemies* enemyGirl;
+    Enemies* enemyTYW;
+
+    // function
+    void initCharacter();
+    void initBall();
+    void initEnemies();
+    void initTexture();
+    void initSprite(sf:: RenderTarget* target);
+    void initBackGround();
 
 public:
 
-        GameState(sf::RenderWindow* window, std:: stack<State*>* states);  // constructor
+    GameState(sf::RenderWindow* window, std:: stack<State*>* states);  // constructor
 
-        virtual ~GameState();  // destructor
+    virtual ~GameState();  // destructor
 
-        // functions
-        void endstate();
-        void updateInput(const float& dt);
-        void updatePlayer();
-        void updateBallCollision();
-        void updateBall();
-        void update(const float& dt);
-        void render(sf::RenderTarget* target = nullptr);
+    // functions
+    void endstate();
+    void updateInput(const float& dt);
+    void updatePlayer();
+    void updateEnemy();
+    void checkSmash(int character);
+    void updateBallPlayerCollision();
+    void updatePlayerEnemiesCollision(sf::RenderTarget* target);
+    void updateBall(const float& dt);
+    void update(const float& dt);
+    void render(sf::RenderTarget* target = nullptr);
+
+
+
 };
 
 #endif //PD110_1_FP_GAMESTATE_H
